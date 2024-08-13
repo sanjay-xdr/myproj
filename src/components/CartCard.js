6a5e003ee9a27ac2b6f7266c737138318db7d963
Sanjay Jaiswal <sanjayjaiswal586@gmail.com>
2024-08-13 08:00:46 +0530

    adding remove from cart

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -23,6 +23,15 @@ export const CartCard = ({title, description, price, image,_id,quatity}) => {
             payload: {  _id },
           })
     }
+
+    const removeCartProduct=(id)=>{
+        dispatch({
+            type: "REMOVE_FROM_CART",
+            payload: {  _id },
+          })
+
+
+    }
   return (
     <div>
 
@@ -33,10 +42,11 @@ export const CartCard = ({title, description, price, image,_id,quatity}) => {
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
         <p>Price: {quatity * price}</p>
-        <button>Remove Item</button>
-      <div>
+    
+      <div className='m-4'>
       <button className='border border-grey-100 h-6 w-6 text-center' onClick={()=>decrementCartItem(_id)}>- </button>  <span>{quatity}</span> <button className='border border-grey-100 h-6 w-6 text-center' onClick={()=>incrementCartItem(_id)}>+</button>
       </div>
+      <button className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900' onClick={()=>removeCartProduct(_id)}>Remove Item</button>
     </div>
 </a>
 
